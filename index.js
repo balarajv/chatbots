22b3f4bf5176705942157c9ec806a11460d10be3
balarajv <balarajv12@gmail.com>
2016-11-17 18:58:34 -0500

    'helloworld'

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/webhook/', function (req, res) {
     //res.send(req.query['hub.challenge'])
 })
 
-
+/*
 app.post('/webhook/', function (req, res) {
     messaging_events = req.body.entry[0].messaging
     for (i = 0; i < messaging_events.length; i++) {
@@ -38,7 +38,7 @@ app.post('/webhook/', function (req, res) {
     }
     res.sendStatus(200)
 })
-
+*/
 var token = "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=EAAIvacFZAdHcBAH6Kjs93WZCIqYKcY3VCO1Mlo7ZAw3bph0KRLUfLvlwjqNa37zyKlvNmo41JUglfJJlA2ZBRHMAtFu0FulGYQoNv1OflfYLZACJ9umeRkBU8mlDRvNpJ9CD3jCTOQ39urAyGrZCE0d2PlruNojV4w43y5CLbeZCAZDZD"
 
 function sendTextMessage(sender, text) {
@@ -61,6 +61,94 @@ function sendTextMessage(sender, text) {
         }
     })
 }
+
+function sendGenericMessage(sender) {
+    messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "First card",
+                    "subtitle": "Element #1 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.messenger.com",
+                        "title": "web url"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+/*app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            if (text === 'Generic') {
+                sendGenericMessage(sender)
+                continue
+            }
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})*/
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            if (text === 'Generic') {
+                sendGenericMessage(sender)
+                continue
+            }
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+        if (event.postback) {
+            text = JSON.stringify(event.postback)
+            sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+            continue
+        }
+    }
+    res.sendStatus(200)
+})
 
 // Spin up the server
 app.listen(app.get('port'), function() {
