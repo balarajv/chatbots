2fd91ad9da8886a889c931b269180ef1b0533f89
balarajv <balarajv12@gmail.com>
2016-11-19 04:25:09 -0500

    Added folder for crawler, deleted curl

---
+++ Machine leanrning/Specific-purpose-Crawler/HelperrClasses.py
@@ -0,0 +1,14 @@
+class AgentDetails(object):
+	def __init__(self, name, phone_no, location, houses_sold, ratings,  active_listings):
+		self.name = name
+		self.phone_no = phone_no
+		self.location = location
+		self.houses_sold = houses_sold
+		self.ratings = ratings
+		self.active_listings = active_listings
+
+class OpenHouseDetails(object):
+	pass
+
+class mortgageAgentDetails(object):
+	pass
---
+++ Machine leanrning/Specific-purpose-Crawler/elastic-search-helper.py
@@ -0,0 +1,4 @@
+from elasticsearch import Elasticsearch
+
+class ElasticSearchHelper(object):
+	
